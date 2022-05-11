e94332964bfb7ee348155fea8c2ce3527ba652e5
Vyacheslav Golovin <v.s.golovin@gmail.com>
2022-05-11 18:36:57 +0300

    Working DQN

--- hw01_lunar_lander/agent.py
+++ hw01_lunar_lander/agent.py
@@ -1,13 +1,14 @@
-import random
 import numpy as np
-import os
 import torch
 
 
 class Agent:
     def __init__(self):
         self.model = torch.load(__file__[:-8] + "/agent.pkl")
-        
-    def act(self, state):
-        return 0 # TODO
 
+    def act(self, state):
+        state = torch.tensor(state)
+        self.model.eval()
+        with torch.no_grad():
+            output = self.model(state).detach().numpy()
+        return np.argmax(output)
--- hw01_lunar_lander/train.py
+++ hw01_lunar_lander/train.py
@@ -1,3 +1,4 @@
+import os
 from gym import make
 import numpy as np
 import torch
@@ -14,7 +15,10 @@ STEPS_PER_UPDATE = 4
 STEPS_PER_TARGET_UPDATE = STEPS_PER_UPDATE * 1000
 BATCH_SIZE = 128
 LEARNING_RATE = 5e-4
-BUFFER_SIZE = 2048
+BUFFER_SIZE = int(1e5)
+# DEVICE = torch.device('cuda:0' if torch.cuda.is_available() else 'cpu')
+DEVICE = torch.device('cpu')
+SEED = 42
 
 
 class DQN:
@@ -47,13 +51,14 @@ class DQN:
     def train_step(self, batch):
         # Use batch to update DQN's network.
         s, a, s1, r, d = map(
-            lambda t: torch.tensor(np.asarray(t)),
+            lambda t: torch.tensor(np.asarray(t), device=DEVICE),
             zip(*batch)
         )
+        self.model.train()
         Q_sa = self.model(s).gather(1, a.view(-1, 1))
-        Q_s1a1 = self.target_model(s1).max(1)[0]
+        Q_s1a1 = self.target_model(s1).detach().max(1)[0]
         with torch.no_grad():
-            target = r.to(torch.float32)
+            target = r.to(dtype=torch.float32, device=DEVICE)
             target[~d] += GAMMA * Q_s1a1[~d]
         loss = F.mse_loss(Q_sa, target.unsqueeze(1))
         self.optimizer.zero_grad()
@@ -65,15 +70,18 @@ class DQN:
         # You may use copy.deepcopy to do this or
         # assign a values of network parameters via PyTorch methods.
         self.target_model.load_state_dict(self.model.state_dict())
+        for param in self.target_model.parameters():
+            param.requires_grad_(False)
 
     def act(self, state, target=False):
         # Compute an action. Do not forget to turn state to a Tensor
         # and then turn an action to a numpy array.
         state = np.array(state)
-        state = torch.tensor(state)
+        state = torch.tensor(state, device=DEVICE)
+        self.model.eval()
         with torch.no_grad():
             action = self.model(state).max(0)[1]
-        return action.cpu().numpy()
+        return action.cpu().detach().numpy()
 
     def update(self, transition):
         # You don't need to change this
@@ -106,14 +114,27 @@ def evaluate_policy(agent, episodes=5):
 
 def create_model(state_dim, action_dim):
     return nn.Sequential(
-        nn.Linear(state_dim, state_dim),
+        nn.Linear(state_dim, 64),
         nn.ReLU(),
-        nn.Linear(state_dim, action_dim),
-    )
+        nn.Linear(64, 64),
+        nn.ReLU(),
+        nn.Linear(64, action_dim),
+    ).to(DEVICE)
 
 
 if __name__ == "__main__":
+    random.seed(SEED)
+    os.environ['PYTHONHASHSEED'] = str(SEED)
+    np.random.seed(SEED)
+    torch.manual_seed(SEED)
+    torch.cuda.manual_seed(SEED)
+    torch.backends.cudnn.deterministic = True
+    torch.use_deterministic_algorithms(True)
+    # still random :'(
+
     env = make("LunarLander-v2")
+    env.action_space.seed(SEED)
+    env.seed(SEED)
     dqn = DQN(state_dim=env.observation_space.shape[0],
               action_dim=env.action_space.n)
     eps = 0.1
@@ -127,9 +148,8 @@ if __name__ == "__main__":
 
         state = next_state if not done else env.reset()
 
-
     for i in range(TRANSITIONS):
-        #Epsilon-greedy policy
+        # Epsilon-greedy policy
         if random.random() < eps:
             action = env.action_space.sample()
         else:
@@ -142,5 +162,6 @@ if __name__ == "__main__":
 
         if (i + 1) % (TRANSITIONS//100) == 0:
             rewards = evaluate_policy(dqn, 5)
-            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, Reward std: {np.std(rewards)}")
+            print(f"Step: {i+1}, Reward mean: {np.mean(rewards)}, "
+                  + f"Reward std: {np.std(rewards)}")
             dqn.save()
